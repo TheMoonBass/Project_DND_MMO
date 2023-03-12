65345c420a44ae320ecb36826d3d590177e6bd28
TheMoonBass <pmoon543@gmail.com>
2023-03-12 18:37:14 -0500

    Updated materials.py and added the monster class

--- material.py
+++ material.py
@@ -1,8 +1,9 @@
 class Material:
-    def __init__(self, name: str, desc: str, rarity: str, shop_sell_price: int, lvl: int):
+    def __init__(self, name: str, desc: str, rarity: str, sell_price: int, buy_price: int, lvl: int):
         self.name = name                                #Material name
         self.desc = desc                                #The material description
         self.rarity = rarity                            #The rarity of the item (common, uncommon, rare, very rare, legendary)
-        self.shop_sell_price = shop_sell_price          #The price the item is available from a shop within the world. If the item is unavailable from a shop, this should be 0
+        self.sell_price = sell_price                    #The price an item can be sold to a shop for
+        self.buy_price = buy_price                      #The price an item can be bought for from a shop
         self.lvl = lvl                                  #The item level (Should be equal to the level of the monster it would be obtained from, the zone it came from if gathered, or the quest it came from if it was a quest reward)
-
+        return
---
+++ monster.py
@@ -0,0 +1,11 @@
+class Monster:
+    def __init__(self, name: str, cr: float, hp: int, ac: int, speed: int, stats: dict, abilities: dict, actions: dict):
+        self.name = name                #The name of the monster
+        self.cr = cr                    #The challenge rating of the monster
+        self.hp = hp                    #The health (Hit Points) of the monster
+        self.ac = ac                    #Armor class of the monster
+        self.speed = speed              #The amount the monster can move per round of combat
+        self.stats = stats              #STR, DEX, CON, INT, WIS, CHA
+        self.abilities = abilities      #Any special abilites that the monster can use.
+        self.actions = actions          #Attacks that the monster can perform.
+        return
